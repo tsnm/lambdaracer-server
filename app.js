02e8e13a0eed9f248cc47f1b267c9ed983b75948
= <=>
2012-07-20 22:37:44 +0200

    revert

--- app.js
+++ app.js
@@ -242,17 +242,7 @@ var getLeaderBoardData = function (callback) {
     if(err) {
       callback({ err: 'there was an error for Player.where()' }, undefined);
     } else {
-      console.log("DB RESULT");
-      console.log(result);
-      console.log(result.length());
-
-      var mail = message;
-      mail.subject = "Anzahl Spieler";
-      mail.text = result.length();
-
-      smtpTransport.sendMail(mail, function(error, response) {
-          callback(undefined, result);
-      });
+      callback(undefined, result);
     }
   });
 };
